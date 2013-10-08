d8717f4481950598fec4f863f10de4e8e4703a2c
Bogdan Vladu <vladu.bogdan@gmail.com>
2013-10-08 06:29:15 +0300

    Updated "Scenes" project example. Now the scene change is done on touch and not with a timer. Added text on screen to better help the user understand the project. Added pragma-mark comments to better visualise functions categories in Animo Code.

--- platino/Engine/Scenes/Resources/NextScene.js
+++ platino/Engine/Scenes/Resources/NextScene.js
@@ -5,6 +5,7 @@
 		var scene = platino.createScene();
 		var redSprite = null;
 
+        //pragma-mark - EVENTS METHODS
 		var onSceneActivated = function(e) {
 
 			// ---- create sprites, add listeners, etc. ----
@@ -17,7 +18,15 @@
 			});
 			redSprite.color(1, 0, 0);
 			scene.add(redSprite);
+			
 			game.startCurrentScene();
+			
+			var text = platino.createTextSprite({text:'Touch to go to previous scene. ', fontSize:14});
+            text.textAlign = Ti.UI.TEXT_ALIGNMENT_CENTER;
+            text.color(1, 1, 1);
+            text.center = { x:game.screen.width  * 0.5, 
+                            y:game.screen.height * 0.5};
+            scene.add(text);
 		};
 
 		var onSceneDeactivated = function(e) {
@@ -32,7 +41,20 @@
 				redSprite = null;
 			}
 		};
+		
+		var onSceneChange = function(e) {
+            // load the module for the scene we want to transition to (in this case, NextScene.js)
+            var MainScene = require('MainScene');
+				
+			// creates an easily accessible reference to the soon-to-be current scene
+			game.currentScene = new MainScene(window, game);
+				
+			// pop the currently shown scene (MainScene.js) from the stack
+			// and replace it with the new current scene (NextScene.js)
+			game.replaceScene(game.currentScene);
+		};
 
+        game.addEventListener('touchstart', onSceneChange);
 		scene.addEventListener('activated', onSceneActivated);
 		scene.addEventListener('deactivated', onSceneDeactivated);
 		return scene;
