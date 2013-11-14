90e410b150a8954f7a9289da040d482e6d3f8cd6
Kota Iguchi <developer@infosia.co.jp>
2013-11-14 15:51:31 +0900

    Add Platino Box2d Physics module sample

---
+++ platino/Physics/PlatinoBox2dModuleSample/Resources/app.js
@@ -0,0 +1,31 @@
+/*
+ * Single Window Application Template:
+ * A basic starting point for your application.  Mostly a blank canvas.
+ * 
+ * In app.js, we generally take care of a few things:
+ * - Bootstrap the application with any data we need
+ * - Check for dependencies like device type, platform version or network connection
+ * - Require and open our top-level UI component
+ *  
+ */
+
+//bootstrap and check dependencies
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
+}
+
+// This is a single context application with multiple windows in a stack
+(function() {
+	//render appropriate components based on the platform and form factor
+	var osname = Ti.Platform.osname,
+		version = Ti.Platform.version,
+		height = Ti.Platform.displayCaps.platformHeight,
+		width = Ti.Platform.displayCaps.platformWidth;
+	
+	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
+	//yourself what you consider a tablet form factor for android
+	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
+	
+	var Window = require('ui/tablet/ApplicationWindow');
+	new Window().open();
+})();
